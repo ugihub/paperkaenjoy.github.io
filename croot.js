d1185b971f034471b13c30611082e3c500c83fdc
Rolly Maulana Awangga <awangga@gmail.com>
2025-02-19 13:25:50 +0700

    kasih komentar

--- croot.js
+++ croot.js
@@ -4,4 +4,4 @@ import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js
 await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
 
 
-Swal.fire('Hehe', 'Kalem bray can hudang keneh', 'info');
+Swal.fire('Hehe', 'Kalem bray can hudang keneh', 'info');//success,warning,info,question
