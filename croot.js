08339957d57ed1536fe4d5420e437337b6e7cf0a
Rolly Maulana Awangga <awangga@gmail.com>
2025-02-19 13:18:52 +0700

    kurang titik koma

--- croot.js
+++ croot.js
@@ -1,4 +1,4 @@
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
-import "https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css"
+import "https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css";
 
 Swal.fire('Hehe', 'Kalem bray can hudang keneh', 'info');
