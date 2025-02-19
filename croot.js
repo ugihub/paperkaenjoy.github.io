d02ea01e345f482620d1dc69628692d71287a5e1
Rolly Maulana Awangga <awangga@gmail.com>
2025-02-19 13:51:32 +0700

    perbaikan pix

--- croot.js
+++ croot.js
@@ -1,10 +1,5 @@
 import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.3/element.js";
-import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
-
 await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
-
-
-Swal.fire('Hehe', 'Kalem bray can hudang keneh', 'info');//success,warning,info,question
 
 
 await addCSS("./css/main.css")
