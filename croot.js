b5513a6c50dfc6a1019fab2e078d62ccb651ab94
Rolly Maulana Awangga <awangga@gmail.com>
2025-02-19 14:06:39 +0700

    cobaan

--- croot.js
+++ croot.js
@@ -1,13 +1,16 @@
-import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.3/element.js";
+import {addCSS,renderHTML} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.3/element.js";
 await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
 
 
 await addCSS("./css/main.css")
 
-document.addEventListener("DOMContentLoaded", function() {
+renderHTML('content', 'html/home.html', afterContentLoad);
+
+function afterContentLoad() {
     let video = document.getElementById("myVideo");
     video.setAttribute("controls", "true");
     video.setAttribute("autoplay", "true");
     video.muted = true;
     video.play(); // Memaksa video untuk diputar
-});
+}
+
