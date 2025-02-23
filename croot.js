9853218b48a155f66455e7d6bb9c476a234232e1
Rolly Maulana Awangga <awangga@gmail.com>
2025-02-23 15:58:32 +0700

    main css belum aktip

--- croot.js
+++ croot.js
@@ -3,8 +3,9 @@ await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css")
 
 
 await addCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css");
-await addCSS("./css/header.css");
 await addCSS("./css/body.css");
+await addCSS("./css/header.css");
+await addCSS("./css/main.css");
 
 renderHTML('header', 'html/header/home.html', afterHeaderLoad);
 renderHTML('main', 'html/main/home.html', afterContentLoad);
