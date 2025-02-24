25e7b920d29db847ddb0cf0962e72858eb09e302
raffzhm <rafiazhim.ra@gmail.com>
2025-02-24 20:49:17 +0700

    ganti nama section slider jadi preview

--- croot.js
+++ croot.js
@@ -1,7 +1,7 @@
 import {addCSS,renderHTML,onClick,container} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.3/element.js";
 import {afterHeaderLoad} from "/js/header/home.js";
 import {afterContentLoad} from "/js/main/home.js";
-import {afterSliderLoad} from "/js/slider/home.js";
+import {afterPreviewLoad} from "/js/preview/home.js";
 
 await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
 
@@ -11,11 +11,11 @@ await addCSS("./css/body.css");
 await addCSS("./css/header.css");
 await addCSS("./css/main.css");
 await addCSS("./css/product.css");
-await addCSS("./css/slider.css");
+await addCSS("./css/preview.css");
 await addCSS("./css/footer.css");
 
 renderHTML('header', 'html/header/home.html', afterHeaderLoad);
 renderHTML('main', 'html/main/home.html', afterContentLoad);
 renderHTML('product', 'html/product/home.html');
-renderHTML('slider', 'html/slider/home.html', afterSliderLoad);
+renderHTML('preview', 'html/preview/home.html', afterPreviewLoad);
 renderHTML('footer', 'html/footer/home.html');
