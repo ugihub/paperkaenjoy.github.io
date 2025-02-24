e54edf403e1e0e7df89c4fe2ab0de7631fe743dd
raffzhm <rafiazhim.ra@gmail.com>
2025-02-24 15:50:52 +0700

    footer

--- croot.js
+++ croot.js
@@ -1,6 +1,7 @@
 import {addCSS,renderHTML,onClick,container} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.3/element.js";
 import {afterHeaderLoad} from "/js/header/home.js";
 import {afterContentLoad} from "/js/main/home.js";
+// import {afterFooterLoad} from "/js/footer/home.js";
 
 await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
 
@@ -9,6 +10,8 @@ await addCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.
 await addCSS("./css/body.css");
 await addCSS("./css/header.css");
 await addCSS("./css/main.css");
+await addCSS("./css/footer.css");
 
 renderHTML('header', 'html/header/home.html', afterHeaderLoad);
 renderHTML('main', 'html/main/home.html', afterContentLoad);
+renderHTML('footer', 'html/footer/home.html');
