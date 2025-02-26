42ce4b4ae712d7541aeed51956d53b942644de36
Rolly Maulana Awangga <awangga@gmail.com>
2025-02-26 07:38:44 +0700

    ubahmaintenance mode video

--- croot.js
+++ croot.js
@@ -1,21 +1,20 @@
 import {addCSS,renderHTML,onClick,container} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.3/element.js";
-import {afterHeaderLoad} from "/js/header/home.js";
-import {afterContentLoad} from "/js/main/home.js";
-import {afterPreviewLoad} from "/js/preview/home.js";
 
-await addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
+await addCSS("./css/video.css")
 
+renderHTML('content', 'html/main/video.html', afterContentLoad);
 
-await addCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css");
-await addCSS("./css/body.css");
-await addCSS("./css/header.css");
-await addCSS("./css/main.css");
-await addCSS("./css/product.css");
-await addCSS("./css/preview.css");
-await addCSS("./css/footer.css");
+function afterContentLoad() {
+    let video = document.getElementById("myVideo");
+    video.setAttribute("autoplay", "true");
+    video.muted = true;
+    video.play(); // Memaksa video untuk diputar
+    onClick('unmuteButton', toggleMute);
+}
 
-renderHTML('header', 'html/header/home.html', afterHeaderLoad);
-renderHTML('main', 'html/main/home.html', afterContentLoad);
-renderHTML('product', 'html/product/home.html');
-renderHTML('preview', 'html/preview/home.html', afterPreviewLoad);
-renderHTML('footer', 'html/footer/home.html');
+function toggleMute(button) {
+    console.log("Tombol diklik!");
+    let video = document.getElementById("myVideo");
+    video.muted = !video.muted;
+    button.textContent = video.muted ? "Unmute" : "Mute";
+}
